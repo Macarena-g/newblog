bcaac5a4415aae04613c6290ef9d07a409175bf0
Macarena <macarena-g@outlook.es>
2025-05-15 11:09:37 -0700

    githubconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://macarenaguzman.github.io',
+	base: '/newblog',
 	integrations: [mdx(), sitemap()],
 });
